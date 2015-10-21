e2a15079c04831ad699507a418516c29b2233c6f
baiyangx <350916161@qq.com>
2015-10-21 22:06:14 +0800

    C Programme

--- ZOJ3809/main.c
+++ ZOJ3809/main.c
@@ -9,7 +9,22 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    int T,n,sum,i,j;
+    int a[8845] = {0};
+    scanf("%d",&T);
+    while(T-->0){
+        sum = 0;
+        scanf("%d",&n);
+        for(i=0;i<n;i++)
+            scanf("%d",&a[i]);
+        a[n]=8845;
+        for(j=1;j<=n-1;j++){
+            if (a[j]>a[j-1]&&a[j]>a[j+1]) {
+                sum++;
+            }
+            
+        }
+        printf("%d\n",sum);
+    }
     return 0;
 }
